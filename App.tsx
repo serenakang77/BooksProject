013c2521901295bf8426e47a1bd4a47acbb35c11
serenakang <serenakang77@gmail.com>
2022-11-30 11:57:03 -0500

    Rendering booklist

--- App.tsx
+++ App.tsx
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources"
+import useColorScheme from "./hooks/useColorScheme"
+import Navigation from "./navigation"
+
+const API_KEY =
+  "hongqiao::stepzen.net+1000::c40900e31e206ed64cedd5611637622c3e411cd2e01efd2efbb4dfe424fad5cc"
+
+const client = new ApolloClient({
+  uri: "https://hongqiao.stepzen.net/api/kindled-aardwolf/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+})
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
-    );
+    )
   }
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,71 @@
-import { StyleSheet } from "react-native"
+import { ActivityIndicator, StyleSheet, FlatList } from "react-native"
 
-import EditScreenInfo from "../components/EditScreenInfo"
 import { Text, View } from "../components/Themed"
-import { RootTabScreenProps } from "../types"
+import { gql, useQuery } from "@apollo/client"
+import BookItem from "../components/BookItem"
+
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  })
+
+  console.log(data)
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
+      {loading && <ActivityIndicator />}
+      {error && (
+        <>
+          <Text>Error fetching books</Text>
+          <Text>{error.message}</Text>
+        </>
+      )}
+      <FlatList
+        data={data?.googleBooksSearch?.items || []}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <BookItem
+            book={{
+              image: item.volumeInfo.imageLinks?.thumbnail,
+              title: item.volumeInfo.title,
+              authors: item.volumeInfo.authors,
+              isbn: item.volumeInfo.industryIdentifiers[0].identifier,
+            }}
+          />
+        )}
+      />
     </View>
   )
 }
@@ -15,8 +73,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 10,
   },
   title: {
     fontSize: 20,
